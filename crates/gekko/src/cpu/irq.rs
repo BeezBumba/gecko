7447db4950831c8df16deba6103e0ce3f6ec2ac7
Layle <me@layle.dev>
2026-03-21 11:09:56 +0100

    refactor syscall

--- crates/gekko/src/cpu/irq.rs
+++ crates/gekko/src/cpu/irq.rs
@@ -49,4 +49,30 @@ impl Gekko {
 
         tracing::debug!(addr = format!("{:08X}", self.cpu.pc), "IRQ triggered");
     }
+
+    pub fn cause_syscall_interrupt(&mut self) {
+        let base: u32 = if self.cpu.msr.exception_prefix() { 0xFFF0_0000 } else { 0 };
+
+        self.cpu.spr.srr0 = Srr0::from(self.cpu.cia.wrapping_add(4));
+        self.cpu.spr.srr1 = chapa::extract_bits!(self.cpu.msr; 0, 5..=9, 16..=31).raw();
+
+        self.cpu.msr = self
+            .cpu
+            .msr
+            .with_pow(false)
+            .with_fp(false)
+            .with_be(false)
+            .with_dr(false)
+            .with_fe1(false)
+            .with_pm(false)
+            .with_ee(false)
+            .with_fe0(false)
+            .with_ri(false)
+            .with_pr(false)
+            .with_se(false)
+            .with_ir(false)
+            .with_le(self.cpu.msr.ile());
+
+        self.cpu.nia = base | IRQ_SYSTEM_CALL;
+    }
 }
